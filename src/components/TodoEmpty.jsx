823dd2138d7c17cf0263a47b76a3ca5c91de5aca
AdalZanabria <adal.zanabria@gmail.com>
2022-05-25 21:20:18 -0500

    Center text.

--- src/components/TodoEmpty.jsx
+++ src/components/TodoEmpty.jsx
@@ -3,7 +3,7 @@ import { ArrowDownIcon } from "@heroicons/react/solid";
 
 function TodoEmpty() {
   return (
-    <div className="flex flex-col justify-center items-center text-violet-600/80">
+    <div className="flex flex-col justify-center items-center text-violet-600/80 text-center">
       <p>No se encontró ningún To-Do</p>
       <p>Puedes crear uno haciendo click en este botón</p>
       <ArrowDownIcon className="h-5 w-5 animate-bounce m-2" />
