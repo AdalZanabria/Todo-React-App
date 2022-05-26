abe43e90e76ade61763f59dd3f51a050b3b7ffc5
AdalZanabria <adal.zanabria@gmail.com>
2022-05-25 21:33:15 -0500

    Cambio de estilos en searchbos.

--- src/components/TodoSearch.jsx
+++ src/components/TodoSearch.jsx
@@ -11,8 +11,8 @@ function TodoSearch() {
 
   return (
     <input
-      className="min-w-[50%] m-4 rounded-lg p-2 outline outline-pink-200 shadow-lg focus:outline-pink-400 focus:scale-110 focus:shadow-xl transition ease-in-out duration-300 text-violet-600"
-      placeholder="Busca un To-Do"
+      className="min-w-[50%] m-4 rounded-lg p-2 outline outline-pink-200 shadow-lg focus:outline-pink-400 focus:scale-110 focus:shadow-xl transition ease-in-out duration-300 text-violet-600 placeholder:italic placeholder:text-violet-500"
+      placeholder="Buscar un To-Do"
       value={searchValue}
       onChange={onSearchValueChange}
     />
